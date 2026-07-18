8be386183b6cd54743454d85b0675a429b104104
Malcolm <138268464+mrkmnns@users.noreply.github.com>
2026-07-18 23:43:54 +0800

    Delete import_data.sql

--- import_data.sql
+++
@@ -1,5 +0,0 @@
-\copy hdb_resale FROM 'C:\\hdbdata\\app3.csv' WITH (FORMAT csv, HEADER true);
-\copy hdb_resale(month, town, flat_type, street_name, block, storey_range, floor_area_sqm, flat_model, lease_commence_date, resale_price) FROM 'C:\\hdbdata\\app2.csv' WITH (FORMAT csv, HEADER true);
-\copy hdb_resale(month, town, flat_type, street_name, block, storey_range, floor_area_sqm, flat_model, lease_commence_date, resale_price) FROM 'C:\\hdbdata\\app1.csv' WITH (FORMAT csv, HEADER true);
-\copy hdb_resale FROM 'C:\\hdbdata\\reg2.csv' WITH (FORMAT csv, HEADER true);
-\copy hdb_resale(month, town, flat_type, street_name, block, storey_range, floor_area_sqm, flat_model, lease_commence_date, resale_price) FROM 'C:\\hdbdata\\reg1.csv' WITH (FORMAT csv, HEADER true);
